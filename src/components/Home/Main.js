5f1cc6e4197b505ffed6997bb85f4e54d08192b8
Nikhil Bansal <nikbansal1998@gmail.com>
2022-06-13 12:21:26 +0530

    feat: add home page hero section

--- src/components/Home/Main.js
+++ src/components/Home/Main.js
@@ -1,16 +1,22 @@
 import React from "react";
 import Wrapper from "../Layouts/Wrapper";
 
+import styles from "./Main.module.css";
+import { Link } from "react-router-dom";
+import home_hero_image from "../../img/home_hero_image.jpeg";
+
 function Main(){
     return (
         <Wrapper>
-            <div class="heroContainer">
-                <h2>Design Your Comfort Zone</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque ipsum magni ex, earum aspernatur vitae, et veniam commodi consequuntur quasi totam quas vero laboriosam fugit sequi recusandae sunt voluptatem odit?</p>
-                <a>SHOP NOW</a>
-            </div>
-            <div>
-                <img src="" alt="" />
+            <div class={styles.hero}>
+                <div className={styles.heroContainer}>
+                    <h2>Design Your Comfort Zone</h2>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloribus distinctio pariatur quod consequatur hic molestiae? Dolor architecto repellat eum quod voluptas exercitationem suscipit ab. Id laborum eum eius totam, adipisci nobis nihil! Quis similique modi sapiente sint voluptates minus dolor!</p>
+                    <Link to="#">Shop Now</Link>
+                </div>
+                <div class={styles.heroImgContainer}>
+                    <img src={home_hero_image} alt="" />
+                </div>
             </div>
         </Wrapper>
     )
